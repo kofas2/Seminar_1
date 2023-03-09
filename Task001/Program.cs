3230bca783c78a79295e311d23afa7baca772a8c
kofas2 <55452453+kofas2@users.noreply.github.com>
2023-03-09 12:10:22 +0300

    add task 1

---
+++ Task001/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32 (Console.ReadLine());
+int square = num*num;
+Console.WriteLine("Квадрат введенного числа равен: " + square);
+
+if (num<0)
+{
+    Console.WriteLine("Введено отрицательное число");
+}
+else
+{
+    Console.WriteLine("Введено неотрицательное число");
+}
